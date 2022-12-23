66b93ca0838b55b2de58c96787c5ac48176c8d98
fredfaya <edyfoottv@gmail.com>
2022-12-24 00:11:52 +0100

    fix : resolve pop pup issue

--- src/app/virement-form/virement-form.component.ts
+++ src/app/virement-form/virement-form.component.ts
@@ -1,16 +1,18 @@
 import { VirementRequest } from './../RequestEntities/VirementRequest';
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { TokenStorageService } from "../_services/token-storage.service";
 import { ClientService } from '../_services/Client.service';
 import Swal from 'sweetalert2';
 import { Client } from '../Models/client';
+
+
 @Component({
     selector:'app-virement-form',
     templateUrl:'./virement-form.component.html'
 })
 
- export class VirementFormComponent {
+ export class VirementFormComponent implements OnInit{
   IdentTypes:string[] = ["C.I.N","Passport","Driver License"];
   transfert_type = 1;
   identity_paper_type = 1;
@@ -18,6 +20,7 @@ import { Client } from '../Models/client';
   SearchReceiver=false;
   sendOTP=false;
   OTPVerified =false;
+  listData: Client[] = [];
   
   public client:Client = new Client();
     virementRequest:VirementRequest={
@@ -29,8 +32,6 @@ import { Client } from '../Models/client';
     showNotEnoughCredit:boolean = false;
     constructor(private router: Router, private tokenStorage:TokenStorageService,private clientService :ClientService) {}
     ngOnInit(): void {
-
-      
       }
 
       effectuerVirementClicked(){
@@ -58,5 +59,22 @@ import { Client } from '../Models/client';
 
       finalize(){
 
+      }
+
+      onAddClient(){
+        
+      }
+      public onOpenModal(client : Client, mode: string): void {
+        const container = document.getElementById("mainContainer");
+        const button = document.createElement('button');
+        button.type = 'button';
+        button.style.display = 'none';
+        button.setAttribute('data-bs-toggle', 'modal');
+        if (mode === 'add') {
+          button.setAttribute('data-bs-target', '#addClientModal');
+        }
+        
+        container?.appendChild(button);
+        button.click();
       }
 }
