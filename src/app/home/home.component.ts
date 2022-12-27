d7c6155f56923d3e5a8ad5a65aab1fa926975d20
fredfaya <edyfoottv@gmail.com>
2022-12-27 20:08:45 +0100

    update the design and components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Agent } from '../Models/agent';
+
 
 @Component({
   selector: 'app-home',
@@ -7,11 +9,30 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  user:string|null = '';
+  user:Agent = {
+    firstName : "FAYA",
+    lastName : "Frederic",
+    password : "dqfberbreqg",
+    birthday : new Date("27-06-2001"),
+    email : "fredericfaya@gmail.com",
+    phoneNumber : "0638743853",
+    address : "some where some where",
+    city : "Marrakech",
+    zip_code : 40000,
+    identity_paper_number : "EB2912??",
+    identity_paper_type : "Passeport",
+    gender : "Male",
+    username : "fred001",
+    country : "Morroco",
+    balance : 1000000000
+  };
+
+  isAdmin:boolean;
  
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.isAdmin = true;
   }
 
   goToProfile(){
@@ -25,4 +46,22 @@ export class HomeComponent implements OnInit {
   goToGetClients(){
     this.router.navigate(['/clientHome/liste-clients']);
   }
+
+  goToServeClients(){
+    this.router.navigate(['/serveClient']);
+  }
+
+  goToGetAgents(){
+    this.router.navigate(['/allAgents']);
+  }
+
+  goToAddAgent(){
+    this.router.navigate(['/addAgent']);
+  }
+
+  goToTransfertList(){
+    this.router.navigate(['/clientHome/listTransfert']);
+  }
+
+
 }
